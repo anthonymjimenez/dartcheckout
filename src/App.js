adc154046665185a8a1339a82a3e5fabe1590304
anthonymjimenez <anthonyjim96@gmail.com>
2023-12-06 13:57:59 -0500

    init creation of app and began creating out table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import outShotsData from "./OutShots";
 
-function App() {
+const App = () => {
+  const initialNumber = 170; // Starting number
+  const [currentNumber, setCurrentNumber] = useState(initialNumber);
+  const [outShots, setOutShots] = useState(outShotsData[initialNumber] || []);
+
+  const handleNumberChange = (e) => {
+    const newNumber = parseInt(e.target.value, 10);
+    const updatedNumber = isNaN(newNumber) ? 0 : newNumber;
+
+    setCurrentNumber(updatedNumber);
+    setOutShots(outShotsData[updatedNumber] || []);
+  };
+
+  const handleIncrement = () => {
+    const newNumber = currentNumber + 1;
+    setCurrentNumber(newNumber);
+    setOutShots(outShotsData[newNumber] || []);
+  };
+
+  const handleDecrement = () => {
+    const newNumber = currentNumber - 1;
+    setCurrentNumber(newNumber);
+    setOutShots(outShotsData[newNumber] || []);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row className="mt-3">
+        <Col md={6} className="mx-auto">
+          <Form.Group controlId="formNumber">
+            <Form.Label>Enter Number:</Form.Label>
+            <Form.Control
+              type="number"
+              value={currentNumber}
+              onChange={handleNumberChange}
+              placeholder="Enter a number"
+            />
+          </Form.Group>
+          <Button variant="primary" className="mr-2" onClick={handleDecrement}>
+            -
+          </Button>
+          <Button variant="primary" onClick={handleIncrement}>
+            +
+          </Button>
+          <div className="mt-3">
+            <h5>Possible Out Shots:</h5>
+            <ul>
+              {outShots.map((shot, index) => (
+                <li key={index}>
+                  <strong>{shot.label}:</strong> {shot.value}
+                  {shot.note && (
+                    <small className="text-muted ml-2">({shot.note})</small>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/OutShots.js
@@ -0,0 +1,455 @@
+const outShotsData = {
+  2: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D1", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  3: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S1-D1",
+      note: "This one intimidates a lot of people which is a good reason to practice it",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  4: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D2", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  5: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "S1-D2", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  6: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "D3",
+      note: "Hitting a S3 leaves S1-D1. If you're feeling comfortable in your lead you might consider S2-D2",
+    },
+    { label: "Alternative", value: "S2-D2", note: "" },
+  ],
+  7: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "S3-D2", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  8: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D4", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  9: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "S1-D4", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  10: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D5", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+
+  11: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "S3-D4", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  12: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D6", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  13: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S11-D1",
+      note: "Safety Hitting a S8 still leaves you with a 2 dart finish. Avoid S5-D4 - hitting a S12 or S20 will bust",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  14: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "D7",
+      note: "If you're willing to waste a dart, shooting the 6/10 will leave either a D4 or D2",
+    },
+    { label: "Alternative", value: "S6-D4", note: "" },
+  ],
+  15: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S13-D1",
+      note: "Safety Hitting a S4 or S6 still leaves you with a 2 dart finish. Avoid S7-D4 - hitting a S16 or S19 will bust.",
+    },
+    { label: "Alternative", value: "S11-D2", note: "" },
+  ],
+  16: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D8", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  17: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S13-D2",
+      note: "Safety Hitting a S4 or S6 still leaves you with a 2 dart finish. Avoid S1-D8 - hitting a S20 or S18 will bust",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  18: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "D9",
+      note: "If you're willing to waste a dart, shooting the 6/10 will leave either a D6 or D4",
+    },
+    {
+      label: "Alternative",
+      value: "S10-D4",
+      note: "If you're willing to waste a dart, shooting the 6/10 will leave either a D6 or D4",
+    },
+  ],
+  19: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S11-D4",
+      note: "Safety Hitting a S8 or S14 won't bust.",
+    },
+    {
+      label: "Alternative",
+      value: "S3-D8",
+      note: "Hitting a S17 or T3 still leaves a double out but hitting a S19 will bust",
+    },
+  ],
+  20: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D10", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  21: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S17-D2",
+      note: "Safety Hitting a S3 still leaves a double out.",
+    },
+    {
+      label: "Alternative",
+      value: "S19-D1",
+      note: "Hitting a S7 or S3 still leaves a double out",
+    },
+  ],
+  22: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D11", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  23: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S19-D2",
+      note: "Safety Hitting a S7 or S3 still leaves a good double out.",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  24: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D12", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  25: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S17-D4",
+      note: "Safety Hitting a S3 still leaves a double out",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  26: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "D13",
+      note: "",
+    },
+    {
+      label: "Alternative",
+      value: "S6-D10",
+      note: "If you're willing to waste a dart, shooting the 6/10 will leave either a D10 or D8",
+    },
+  ],
+  27: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S19-D4",
+      note: "Safety Hitting a S7 or S3 still leaves a good double out",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  28: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D14", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  29: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S17-D6",
+      note: "Safety Hitting a S3 still leaves a double out. Option #2 - S19-D5. Hitting a T7 or T3 will leave a double out",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  30: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D15", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  31: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S19-D6",
+      note: "Safety Hitting a S7 or S3 still leaves a good double out",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  32: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D16", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  33: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S1-D16",
+      note: "Option #1 Hitting a S2 leaves a D15. Option #2 Hitting a T6 leaves a D15",
+    },
+    { label: "Alternative", value: "", note: "" },
+    {
+      label: "Two Dart Safety",
+      value: "S17-D8",
+      note: "Consider if you only have two darts in hand",
+    },
+  ],
+  34: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D17", note: "" },
+    { label: "Alternative", value: "", note: "" },
+    {
+      label: "Two Dart Safety",
+      value: "S3-D16 or S19-D8",
+      note: "Split Decision - Shoot the wire between 3 & 19 which will leave a D16 or D8. Beware D19 or T19 will bust",
+    },
+  ],
+  35: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S3-D16",
+      note: "Split Decision - Shoot the wire between 3 & 19 which will leave a D16 or D8. Beware D19 or T19 will bust",
+    },
+    { label: "Alternative", value: "S19-D8", note: "" },
+  ],
+  36: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D18", note: "" },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  37: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S5-D16",
+      note: "",
+    },
+    { label: "Alternative", value: "", note: "" },
+    {
+      label: "Two Dart Safety",
+      value: "S17-D10",
+      note: "Safety S3 or T3 will leave a one-dart finish. Beware T17 will bust",
+    },
+  ], // what needs work still
+  38: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D19", note: "" },
+    { label: "Alternative", value: "", note: "" },
+    {
+      label: "Two Dart Safety",
+      value: "7/19 split",
+      note: "Consider if you only have two darts in hand",
+    },
+  ],
+  39: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S7-D16",
+      note: "Option #1 Hitting a S3 leaves a D16. Option #2 Hitting a T3 leaves a D16",
+    },
+    { label: "Alternative", value: "", note: "" },
+    {
+      label: "Two Dart Safety",
+      value: "7/19 split",
+      note: "Consider if you only have two darts in hand",
+    },
+  ],
+  40: [
+    { label: "Professional", value: "" },
+    { label: "Practical", value: "D20", note: "" },
+    { label: "Alternative", value: "", note: "" },
+    {
+      label: "Two Dart Safety",
+      value: "S17-D12",
+      note: "Safety S3 or T3 will leave a one-dart finish",
+    },
+  ],
+  41: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S9-D16",
+      note: "Option #1 Hitting a S5 leaves a D16. Option #2 Hitting a T1 leaves a D16",
+    },
+    { label: "Alternative", value: "", note: "" },
+    {
+      label: "Two Dart Safety",
+      value: "S17-D12",
+      note: "Safety S3 or T3 will leave a one-dart finish",
+    },
+  ],
+  42: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "T14-D20",
+      note: "Option #1 Hitting a T14 leaves a D20. Option #2 Hitting a T11 leaves a D20",
+    },
+    { label: "Alternative", value: "", note: "" },
+    {
+      label: "Two Dart Safety",
+      value: "S3-D20",
+      note: "Hitting a T14 leaves a D20. If you miss the treble, hitting a single 3 will leave a double out",
+    },
+  ],
+  43: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S3-D20",
+      note: "Hitting a T14 leaves a D20. If you miss the treble, hitting a single 3 will leave a double out",
+    },
+    { label: "Alternative", value: "", note: "" },
+    {
+      label: "Two Dart Safety",
+      value: "S3-D20",
+      note: "Hitting a T14 leaves a D20. If you miss the treble, hitting a single 3 will leave a double out",
+    },
+  ],
+  44: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "T12-D16",
+      note: "Option #1 Hitting a T12 leaves a D16. Option #2 Hitting a T10 leaves a D16",
+    },
+    { label: "Alternative", value: "", note: "" },
+    {
+      label: "Two Dart Safety",
+      value: "S15-D16",
+      note: "Avoid S17-D16. Hiting the S3 or S2 will not leave a one-dart finish and T17 will bust",
+    },
+  ],
+  45: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "T13-D18",
+      note: "Option #1 Hitting a T13 leaves a D18. Option #2 Hitting a T5 leaves a D20",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  46: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "T14-D17",
+      note: "Option #1 Hitting a T14 leaves a D17. Option #2 Hitting a T6 leaves a D20",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  47: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S15-D16",
+      note: "Option #1 Hitting a S15 leaves a D16. Option #2 Hitting a T2 leaves a D20",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  48: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "T16-D20",
+      note: "Option #1 Hitting a T16 leaves a D20. Option #2 Hitting a T8 leaves a D20",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  49: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S17-D16",
+      note: "Option #1 Hitting a S17 leaves a D16. Option #2 Hitting a T4 leaves a D20",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  50: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "T18-D16",
+      note: "Option #1 Hitting a T18 leaves a D16. Option #2 Hitting a T10 leaves a D20",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  51: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "S19-D16",
+      note: "Option #1 Hitting a S19 leaves a D16. Option #2 Hitting a T2 leaves a D20",
+    },
+    { label: "Alternative", value: "", note: "" },
+  ],
+  // Continue with the rest of the numbers...
+  170: [
+    { label: "Professional", value: "" },
+    {
+      label: "Practical",
+      value: "T20-T20-DB",
+      note: "The highest finish possible and coveted as a significant accomplishment.",
+    },
+    {
+      label: "Alternative",
+      value: "",
+      note: "The highest finish possible and coveted as a significant accomplishment.",
+    },
+  ],
+};
+
+export default outShotsData;
